8adc9a0f960e393940a127750afdab814d905cb4
thomasKERHERVE <titom.kerherve@gmail.com>
2022-04-28 00:03:19 +0200

    Fonction Carre | Distance(entre deux points)

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+from math import *
+
+def Carre (x: int):
+    return x * x
+
+def Distance(x1 : int , y1 : int , x2 : int , y2 : int):
+    x = sqrt(Carre(x2-x1)+(Carre(y2-y1)))
+    return x
+
 def Lecture_Fichier():
     TSP = open('bayg29.tsp', 'r')
 
     Name = TSP.readline().strip().split()[1]
     Type = TSP.readline().strip().split()[1]
-    Comment = TSP.readline().rstrip().split()[1]   #Trouver comment extraire tout le rest ede la ligne ...
+    Comment = TSP.readline().rstrip().split()[1]   #Trouver comment extraire tout le reste de la ligne ...
     Dimension = TSP.readline().strip().split()[1]
     EDGE_WEIGHT_TYPE = TSP.readline().strip().split()[1]
     for line in TSP:
