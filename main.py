e9f2d332232de6ae1710035f0502d5a90990d6ac
thomasKERHERVE <titom.kerherve@gmail.com>
2022-04-27 12:16:25 +0200

    Fonction lecture de fichier problème : -Commentaire pas afficher en entier -Différencier les deux fichier

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
+def type1():
+    print("oui")
+
+def type2():
+    print("oui")
+
 def Lecture_Fichier():
     TSP = open('bayg29.tsp', 'r')
 
     Name = TSP.readline().strip().split()[1]
     Type = TSP.readline().strip().split()[1]
-    Comment = TSP.readline().rstrip().split()[1]   #Trouver comment extraire tout le rest ede la ligne ...
+    Comment = TSP.readline().rstrip().split()[1]   #Trouver comment extraire tout le reste de la ligne ...
     Dimension = TSP.readline().strip().split()[1]
     EDGE_WEIGHT_TYPE = TSP.readline().strip().split()[1]
-    while TSP.readline() != "NODE_COORD_SECTION" or TSP.readline() != EDGE_WEIGHT_SECTION:
+    while TSP.readline() != "NODE_COORD_SECTION" or TSP.readline() != "EDGE_WEIGHT_SECTION":
         TSP.readline()
     if TSP.readline() == "NODE_COORD_SECTION":
-        # Faire appel à une fonction pour le type de Fichier qui ressemble à att48.tsp
+        Type1() # Faire appel à une fonction pour le type de Fichier qui ressemble à att48.tsp
     else:
-        # Faire appel à une fonction pour le type de Fichier comme bayg29.tsp
+        Type2() # Faire appel à une fonction pour le type de Fichier comme bayg29.tsp
     print(Name,Type,Comment,int(Dimension),EDGE_WEIGHT_TYPE)
 
 Lecture_Fichier()
