25d46c84c3031585c05d8d705cbec5642634f9a2
Samuel <samuel.leobon@gmail.com>
2022-04-27 12:17:52 +0200

    Synchrone3

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ def Lecture_Fichier():
     Comment = TSP.readline().rstrip().split()[1]   #Trouver comment extraire tout le rest ede la ligne ...
     Dimension = TSP.readline().strip().split()[1]
     EDGE_WEIGHT_TYPE = TSP.readline().strip().split()[1]
-    while TSP.readline() != "NODE_COORD_SECTION" or TSP.readline() != EDGE_WEIGHT_SECTION:
+    while TSP.readline() != "NODE_COORD_SECTION" or TSP.readline() != "EDGE_WEIGHT_SECTION":
         TSP.readline()
     if TSP.readline() == "NODE_COORD_SECTION":
+        print('Appel extraction coordonnées')
         # Faire appel à une fonction pour le type de Fichier qui ressemble à att48.tsp
     else:
+        print('Appel extraction distances')
         # Faire appel à une fonction pour le type de Fichier comme bayg29.tsp
     print(Name,Type,Comment,int(Dimension),EDGE_WEIGHT_TYPE)
 
