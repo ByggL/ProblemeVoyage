2e2d6e22c376acbd53bdc290ece54ab5042ffa35
Samuel <samuel.leobon@gmail.com>
2022-04-27 14:24:42 +0200

    Boucle de vérif des données qui marche

--- main.py
+++ main.py
@@ -6,14 +6,15 @@ def Lecture_Fichier():
     Comment = TSP.readline().rstrip().split()[1]   #Trouver comment extraire tout le rest ede la ligne ...
     Dimension = TSP.readline().strip().split()[1]
     EDGE_WEIGHT_TYPE = TSP.readline().strip().split()[1]
-    while TSP.readline() != "NODE_COORD_SECTION" or TSP.readline() != "EDGE_WEIGHT_SECTION":
-        TSP.readline()
-    if TSP.readline() == "NODE_COORD_SECTION":
-        print('Appel extraction coordonnées')
-        # Faire appel à une fonction pour le type de Fichier qui ressemble à att48.tsp
-    else:
-        print('Appel extraction distances')
-        # Faire appel à une fonction pour le type de Fichier comme bayg29.tsp
+    for line in TSP:
+        if 'NODE_COORD_SECTION' in line:
+            print('Appel extraction coordonnées')
+            break
+            # Faire appel à une fonction pour le type de Fichier qui ressemble à att48.tsp
+        elif 'EDGE_WEIGHT_SECTION' in line:
+            print('Appel extraction distances')
+            break
+            # Faire appel à une fonction pour le type de Fichier comme bayg29.tsp
     print(Name,Type,Comment,int(Dimension),EDGE_WEIGHT_TYPE)
 
 Lecture_Fichier()
